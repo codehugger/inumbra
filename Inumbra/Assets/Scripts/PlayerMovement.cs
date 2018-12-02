10e4a2198d27a801fdc1c7857890d42331d59ebd
Dennis Maier <dennis.maier@tum.de>
2018-12-02 14:20:48 +0000

    add new character control  scripts

---
+++ Inumbra/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxis("Horizontal") + " and " + Input.GetAxis("Vertical"));
+        float in_x = Input.GetAxis("Horizontal");
+        float in_y = Input.GetAxis("Vertical");
+        float pos_x = transform.position.x + (in_x * speed * Time.deltaTime);
+        float pos_y = transform.position.y + (in_y * speed * Time.deltaTime);
+        Vector3 new_pos = new Vector3(pos_x, pos_y, 0f);
+        Vector3 move_direction = new_pos - transform.position;
+        transform.position = new_pos;
+        if (System.Math.Abs(in_y) > 0.3 || System.Math.Abs(in_x) > 0.3)
+        {
+            float angle = Mathf.Atan2(move_direction.y, move_direction.x) * Mathf.Rad2Deg;
+            GameObject.FindGameObjectWithTag("Legs").transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
---
+++ Inumbra/Assets/Scripts/PlayerOrientation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerOrientation : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
