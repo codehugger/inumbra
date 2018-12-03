4cb350c91f6403078a73c5a12ba8c4040ab1525d
Dennis Maier <dennis.maier@tum.de>
2018-12-03 21:26:11 +0000

    improve enemy movement

--- Inumbra/Assets/Scripts/EnemyMovement.cs
+++ Inumbra/Assets/Scripts/EnemyMovement.cs
@@ -4,56 +4,59 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-	public float speed = 1;
+	public float rotationSpeed = 10;
+	public float movementSpeed = 1;
 	public float directionChangeInterval = 0.5f;
 	public float playerDistance = 3;
 
-	Vector3 target;
+	Quaternion target;
 	float time = 0;
 	float last_time = 0;
 	GameObject player;
 
 	// Use this for initialization
 	void Start () {
-		target = GetRandomDirection();
+		target = GetRandomRotation();
 		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		time += Time.time - last_time;
-		if (time > directionChangeInterval || (transform.position == target))
-		{
-			target = GetRandomDirection();
-			time = 0;
-		}
-		last_time = Time.time;
-		Vector3 new_pos;
+
 		if (Vector3.Distance(player.transform.position, transform.position) < playerDistance)
 		{
-			new_pos = player.transform.position;
+			target = Quaternion.LookRotation(player.transform.position - transform.position, -Vector3.forward);
+			target.x = 0;
+			target.y = 0;
+			time = 0;
 		}
-		else 
+		else if (time > directionChangeInterval)
 		{
-			new_pos = transform.position + target;
+			target = GetRandomRotation();
+			time = 0;
 		}
-        transform.position = Vector3.Lerp(transform.position, new_pos ,Random.Range(1f, 3f) * speed * Time.deltaTime);
-		// var dir = new_pos + target ;//- transform.position;
-        // var angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
-        // transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+		transform.rotation = Quaternion.Lerp(transform.rotation, target, rotationSpeed * Time.deltaTime);
+		transform.position += transform.up * movementSpeed * Time.deltaTime;
+
+		last_time = Time.time;
 	}
 
-	Vector3 GetRandomDirection()
-	{
-		return new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+	Quaternion GetRandomRotation()
+	{	
+		Quaternion random_rotation = Random.rotation;
+		random_rotation.x = 0;
+		random_rotation.y = 0;
+		return random_rotation;
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
-			Debug.Log("DEAD!!");
+			Destroy(gameObject);
 		}
-		Destroy(gameObject);
+		
 	}
 }
