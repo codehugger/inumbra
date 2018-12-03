224b039fb544b988862bbdef2841668cb822be42
Bjarki Gudlaugsson <bjarkigud@gmail.com>
2018-12-03 10:05:17 +0000

    Train movement ... not working

---
+++ Inumbra/Assets/Scripts/HoldCharacter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldCharacter : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.transform.parent = gameObject.transform;
+        Debug.Log("We have collision enter");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        collision.transform.parent = gameObject.transform;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.transform.parent = null;
+        Debug.Log("We have collision exit");
+    }
+}
---
+++ Inumbra/Assets/Scripts/TrainMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainMovement : MonoBehaviour {
+
+    public Vector2 target;
+    public float speed;
+    Vector2 position;
+
+	// Use this for initialization
+	void Start () {
+        position = gameObject.transform.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, target, step);	
+	}
+}
