7e62d397cafdf66019aa1780636ad79d4a507178
carl <carlwrosen@gmail.com>
2018-12-04 18:03:51 +0000

    add roof functionality for train

--- Inumbra/Assets/Scripts/HoldCharacter.cs
+++ Inumbra/Assets/Scripts/HoldCharacter.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class HoldCharacter : MonoBehaviour
 {
+    
+    public GameObject roof;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
         other.gameObject.transform.SetParent(transform);
+
+        //hide roof
+        roof.GetComponent<Renderer>().enabled = false;
         }
+        
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -16,6 +23,10 @@ public class HoldCharacter : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
         other.gameObject.transform.SetParent(null);
+
+        //show roof
+        roof.GetComponent<Renderer>().enabled = true;
+        
         }
     }
 }
