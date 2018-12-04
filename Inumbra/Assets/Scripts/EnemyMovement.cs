69fdf3ab651857331a61db6e4701670e5a5f85de
Dennis Maier <dennis.maier@tum.de>
2018-12-04 16:22:33 +0000

    new enemy behavior

--- Inumbra/Assets/Scripts/EnemyMovement.cs
+++ Inumbra/Assets/Scripts/EnemyMovement.cs
@@ -8,59 +8,105 @@ public class EnemyMovement : MonoBehaviour {
 	public float movementSpeed = 1;
 	public float timeToKill = 2;
 	public float playerDistance = 3;
+	public float fleeSpeed = 5;
 
 	Quaternion target;
 	float time = 0;
-	float last_time = 0;
+	float lastTime = 0;
 	GameObject player;
 	float directionChangeInterval;
 	float exposureTime;
+	int internalState;
+	bool inLantern;
 
 	// Use this for initialization
 	void Start () {
 		target = GetRandomRotation();
 		player = GameObject.FindGameObjectWithTag("Player");
 		directionChangeInterval = Random.value * 3;
+		internalState = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		time += Time.time - last_time;
 
-		if (Vector3.Distance(player.transform.position, transform.position) < playerDistance)
+		UpdateState();
+
+		if (internalState == 2)
 		{
-			target = Quaternion.LookRotation(player.transform.position - transform.position, -Vector3.forward);
-			target.x = 0;
-			target.y = 0;
-			time = 0;
-		}
-		else if (time > directionChangeInterval)
-		{
-			if (Random.Range(0f, 1f) > 0.7f)
+			if (Vector3.Distance(player.transform.position, transform.position) < playerDistance/2)
 			{
-				movementSpeed = 0;
+				if (Random.Range(0f, 1f) > 0.5f)
+				{
+					target = Quaternion.LookRotation(transform.position + transform.right - transform.up, -Vector3.forward);
+				}
+				else
+				{
+					target = Quaternion.LookRotation(transform.position - transform.right - transform.up, -Vector3.forward);
+				}
 			}
 			else
 			{
-				movementSpeed += 1;
+				target = Quaternion.LookRotation(transform.position - transform.up, -Vector3.forward);
 			}
-			target = GetRandomRotation();
 			time = 0;
-			directionChangeInterval = Random.value * 3;
+			movementSpeed = fleeSpeed;
 		}
+		else if (internalState == 1)
+		{
+			target = Quaternion.LookRotation(player.transform.position - transform.position, -Vector3.forward);
+			time = 0;
+		}
+		else 
+		{
+			if (time > directionChangeInterval)
+			{
+				if (Random.Range(0f, 1f) > 0.7f)
+				{
+					movementSpeed = 0;
+				}
+				else
+				{
+					movementSpeed += 0.2f;
+				}
+				target = GetRandomRotation();
+				time = 0;
+				directionChangeInterval = Random.value * 3;
+			}
+		}
+
+		target.x = 0;
+		target.y = 0;
 
 		transform.rotation = Quaternion.Lerp(transform.rotation, target, rotationSpeed * Time.deltaTime);
 		transform.position += transform.up * movementSpeed * Time.deltaTime;
 
-		last_time = Time.time;
+		lastTime = Time.time;
+	}
+
+	void UpdateState()
+	{
+		if (inLantern)
+		{
+			// RETREAT
+			internalState = 2;
+		}
+		else if(Vector3.Distance(player.transform.position, transform.position) < playerDistance)
+		{
+			// ATTACK
+			internalState = 1;
+		}
+		else 
+		{
+			// MOVE
+			internalState = 0;
+		}
 	}
 
 	Quaternion GetRandomRotation()
 	{	
-		Quaternion random_rotation = Random.rotation;
-		random_rotation.x = 0;
-		random_rotation.y = 0;
-		return random_rotation;
+		Quaternion randomRotation = Random.rotation;
+		return randomRotation;
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
@@ -72,14 +118,15 @@ public class EnemyMovement : MonoBehaviour {
 		else if (other.gameObject.tag == "Lantern")
 		{
 			exposureTime = 0;
+			inLantern = true;
 		}	
 	}
 
 	void OnTriggerStay2D(Collider2D other)
 	{
-		if (other.gameObject.tag == "Lantern")
+		// Debug.Log(Physics.Raycast(transform.position, player.transform.position - transform.position, 100));
+		if (other.gameObject.tag == "Lantern")// && !Physics.Raycast(transform.position, player.transform.position - transform.position, 100))
 		{
-			Debug.Log(exposureTime);
 			exposureTime += Time.fixedDeltaTime;
 			if (exposureTime >= timeToKill)
 			{
@@ -89,6 +136,14 @@ public class EnemyMovement : MonoBehaviour {
 			{
 				transform.localScale += new Vector3(0.01f, 0.01f, 0);
 			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Lantern")
+		{
+			inLantern = false;
 		}
 	}
 }
--- Inumbra/Assets/Scripts/HoldCharacter.cs
+++ Inumbra/Assets/Scripts/HoldCharacter.cs
@@ -5,11 +5,17 @@ public class HoldCharacter : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.tag == "Player")
+        {
         other.gameObject.transform.SetParent(transform);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        if (other.gameObject.tag == "Player")
+        {
         other.gameObject.transform.SetParent(null);
+        }
     }
 }
