5ea4577f65b56cb584e54df1127fa8781906b01c
carl <carlwrosen@gmail.com>
2018-12-05 12:01:25 +0000

    start fuel and inventory system

--- Inumbra/Assets/Scripts/TrainMovement.cs
+++ Inumbra/Assets/Scripts/TrainMovement.cs
@@ -7,6 +7,10 @@ public class TrainMovement : MonoBehaviour {
     public Vector2 target;
     public float speed;
     Vector2 position;
+    
+    // FOR ALPHA
+    public bool enoughFuel = false;
+
 
 	// Use this for initialization
 	void Start () {
@@ -18,4 +22,26 @@ public class TrainMovement : MonoBehaviour {
         float step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, target, step);	
 	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Check for fuel and fill tank
+            if(other.GetComponent<PlayerInventoryController>().fuelAmount > 0){
+                enoughFuel = true;
+            }
+        }
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Check for fuel and fill tank
+            
+        }
+    }
+    
+   
 }
