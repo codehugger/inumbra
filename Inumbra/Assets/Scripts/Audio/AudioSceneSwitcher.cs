9b5ed1c06d1d5bff4f6b1a860014ff005ef4bb61
Bjarki Gudlaugsson <bjarkigud@gmail.com>
2018-12-05 15:00:47 +0000

    Added Intro scene

---
+++ Inumbra/Assets/Scripts/Audio/AudioSceneSwitcher.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class AudioSceneSwitcher : MonoBehaviour {
+	public string sceneName;
+	public TextMeshProUGUI firstText;
+	public TextMeshProUGUI secondText;
+	public float transitionDuration;
+
+	void Start() {
+		firstText.color = new Color(0,0,0,0);
+		secondText.color = new Color(0,0,0,0);
+		StartCoroutine(playSoundThenLoad());
+	}
+
+	IEnumerator playSoundThenLoad()
+	{
+		AudioSource audio = GetComponent<AudioSource>();
+
+		Debug.Log("Playing Sound");
+		audio.Play();
+		//yield return new WaitForSeconds(audio.clip.length);
+
+		float textFade = audio.clip.length * 0.25f;
+
+		// fade first text out
+		float elapsed = 0.0f;
+		while (elapsed <= textFade)
+		{
+			firstText.color = Color.Lerp(new Color(0,0,0,0), Color.white, elapsed / textFade);
+			elapsed += Time.deltaTime;
+
+			yield return null;
+		}
+
+		// fade first text out
+		elapsed = 0.0f;
+		while (elapsed <= textFade)
+		{
+			firstText.color = Color.Lerp(Color.white, new Color(0,0,0,0), elapsed / textFade);
+			elapsed += Time.deltaTime;
+
+			yield return null;
+		}
+
+		// fade second text in
+		elapsed = 0.0f;
+		while (elapsed <= textFade)
+		{
+			secondText.color = Color.Lerp(new Color(0,0,0,0), Color.white, elapsed / textFade);
+			elapsed += Time.deltaTime;
+
+			yield return null;
+		}
+
+		// fade second text out
+		elapsed = 0.0f;
+		while (elapsed <= textFade)
+		{
+			secondText.color = Color.Lerp(Color.white, new Color(0,0,0,0), elapsed / textFade);
+			elapsed += Time.deltaTime;
+
+			yield return null;
+		}
+
+		Debug.Log("Loading Scene");
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+	}
+}
