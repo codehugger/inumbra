ca90504d0a4c76ce1bbbbbfa927c29de80a4eca5
carl <carlwrosen@gmail.com>
2018-12-05 17:14:25 +0000

    add trees and house

---
+++ Inumbra/Assets/Scripts/RoofController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofController : MonoBehaviour {
+
+	public GameObject roof;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	 private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+        //hide roof
+        roof.SetActive(false);
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+        //show roof
+        roof.SetActive(true);
+        }
+    }
+}
