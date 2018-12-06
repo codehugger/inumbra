89c8af09699ae731827f17073c8a26c051500f15
Bjarki Gudlaugsson <bjarkigud@gmail.com>
2018-12-06 22:02:46 +0000

    PlayerSpawn script

---
+++ Inumbra/Assets/PlayerSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour {
+
+	public GameObject playerObject;
+	public AudioClip lanternSound;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator TurnLanternOn() {
+		playerObject.SetActive(false);
+		audioSource.PlayOneShot(lanternSound);
+		yield return new WaitForSeconds(lanternSound.length);
+		playerObject.SetActive(true);
+	}
+}
