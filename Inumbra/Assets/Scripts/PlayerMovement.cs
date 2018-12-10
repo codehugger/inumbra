f7d6cef03cf1435e03da6c4c0a70cd3542b57eaa
Bjarki Gudlaugsson <bjarkigud@gmail.com>
2018-12-10 15:51:47 +0000

    Player movement can now be controlled with PS4 controller

--- Inumbra/Assets/Scripts/PlayerMovement.cs
+++ Inumbra/Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,8 @@ public class PlayerMovement : MonoBehaviour
     private GameObject _body;
     private GameObject _legs;
 
+    Vector3 mousePosition;
+
     // Use this for initialization
     void Start()
     {
@@ -28,34 +30,55 @@ public class PlayerMovement : MonoBehaviour
         float in_y = Input.GetAxis("Vertical");
         float pos_x = transform.position.x + (in_x * speed * Time.deltaTime);
         float pos_y = transform.position.y + (in_y * speed * Time.deltaTime);
+
         Vector3 new_pos = new Vector3(pos_x, pos_y, 0f);
 
         //Calculate length of movement for animation state change
         float length = Vector3.Magnitude(new_pos - transform.position);
         animator.SetFloat("Speed", length);
-        
         transform.position = new_pos;
+
         Vector3 dir;
-        if (GameObject.FindGameObjectWithTag("PixelCanvas") != null){
+
+        if (GameObject.FindGameObjectWithTag("PixelCanvas") != null) {
             Vector3 canvas_pos = GameObject.FindGameObjectWithTag("PixelCanvas").GetComponent<Canvas>().transform.position;
-        
             dir = Input.mousePosition - canvas_pos;
         }
         else {
             dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         }
-        float look_angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        Quaternion new_rot = Quaternion.AngleAxis(look_angle, Vector3.forward);
-        _body.transform.rotation = new_rot;
 
-        Quaternion legs_rot = _legs.transform.rotation;
-        float angle_diff = Quaternion.Angle(legs_rot, new_rot);
+        var angH = Input.GetAxis("RightH");
+        var angV = Input.GetAxis("RightV");
+        float angleDiff;
+        Quaternion legsRot = _legs.transform.rotation;
+        Quaternion newRot = _body.transform.rotation;
 
-        if (angle_diff > deadAngle || System.Math.Abs(in_y) > 0 || System.Math.Abs(in_x) > 0)
-        {
-            _legs.transform.rotation = Quaternion.Lerp(legs_rot, new_rot, legTurnSpeed * Time.deltaTime);
+        if (angH != 0 || angV != 0) {
+            var ang = new Vector2(Input.GetAxis("RightH"), Input.GetAxis("RightV"));
+            var angle = Vector2.SignedAngle(ang, Vector2.right);
+            // _body.transform.Rotate(Vector3.left, 45 * Time.deltaTime * speed);
+            // _body.transform.Rotate(0, 0, angle * Time.deltaTime * speed);
+            _body.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+            newRot = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        } else if (mousePosition != Input.mousePosition) {
+            float look_angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            newRot = Quaternion.AngleAxis(look_angle, Vector3.forward);
+            _body.transform.rotation = newRot;
+
+            angleDiff = Quaternion.Angle(legsRot, newRot);
+            // TODO: try to rotate legs towards input axis and not body
         }
-        // TODO: try to rotate legs towards input axis and not body
-        //
+
+        angleDiff = Quaternion.Angle(legsRot, newRot);
+
+        if (angleDiff > deadAngle || System.Math.Abs(in_y) > 0 || System.Math.Abs(in_x) > 0)
+        {
+            _legs.transform.rotation = Quaternion.Lerp(legsRot, newRot, legTurnSpeed * Time.deltaTime);
+        }
+
+        mousePosition = Input.mousePosition;
     }
 }
