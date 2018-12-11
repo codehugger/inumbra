06b10feea28a0caaa21e380f8c3eb4139b61ed59
Dennis Maier <dennis.maier@tum.de>
2018-12-11 15:28:19 +0000

    add lantern sounds

--- Inumbra/Assets/Scripts/LanternController.cs
+++ Inumbra/Assets/Scripts/LanternController.cs
@@ -19,11 +19,22 @@ public class LanternController : MonoBehaviour {
 	[HideInInspector]
 	public float rayIntensity = 0f;
 
+	public AudioClip inAudio;
+	public AudioClip outAudio;
+
 	Vector3 spotlightChangeVector = new Vector3(1, 0, 0);
 	Vector3 aoeChangeVector = new Vector3(0, 1, 0);
 
+	bool focusStarted = false;
+	bool inFocus = false;
+	bool exitFocus = false;
+	bool soundDone = true;
+
+	AudioSource audio;
+
 	// Use this for initialization
 	void Start () {
+		audio = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -31,10 +42,30 @@ public class LanternController : MonoBehaviour {
 		if (Input.GetButton("Fire1") && lightVisual.transform.localScale.x > minRay) {
 			lightVisual.transform.localScale -= spotlightChangeVector * Time.deltaTime * speed;
 			lightCollider.transform.localScale -= aoeChangeVector * Time.deltaTime * speed / 3;
+			float cur_alpha = lightVisual.GetComponent<LightSprite>().Color.a;
+			lightVisual.GetComponent<LightSprite>().Color.a = Mathf.Lerp(cur_alpha, 0.8f, Time.deltaTime * speed/10);
+			if (!focusStarted){
+				focusStarted = true;
+				audio.clip = inAudio;
+				audio.loop = true;
+				exitFocus = false;
+				StartCoroutine(PlayLanternSound());
+			}
+			
 		} else if (!Input.GetButton("Fire1") && lightVisual.transform.localScale.x < maxRay) {
 			lightVisual.transform.localScale += spotlightChangeVector * Time.deltaTime * speed * 3;
 			lightCollider.transform.localScale += aoeChangeVector * Time.deltaTime * speed;
+			float cur_alpha = lightVisual.GetComponent<LightSprite>().Color.a;
+			lightVisual.GetComponent<LightSprite>().Color.a = Mathf.Lerp(cur_alpha, -0.3f, Time.deltaTime * speed/10);
+			if (!exitFocus) {
+				focusStarted = false;
+				exitFocus = true;
+				audio.clip = outAudio;
+				audio.loop = false;
+				StartCoroutine(PlayLanternSound());
+			}
 		}
+
 		if (lightVisual.transform.localScale.x > maxRay) {
 			Vector3 tmp = lightVisual.transform.localScale;
 			tmp.x = maxRay;
@@ -54,9 +85,17 @@ public class LanternController : MonoBehaviour {
 			lightCollider.transform.localScale = tmp;
 		}
 
+		float clamp_alpha = Mathf.Clamp(lightVisual.GetComponent<LightSprite>().Color.a, 0.3f, 0.8f);
+		lightVisual.GetComponent<LightSprite>().Color.a = clamp_alpha;
 		// convert light to normalized rayIntensity
 		float lightX = Mathf.Clamp(lightVisual.transform.localScale.x, minRay, maxRay);
 		rayIntensity = Mathf.Clamp(1 - (lightX-minRay)/(maxRay-minRay), 0.01f, 1.0f);
 		currentDamage = rayIntensity * damagePerSecond;
+	}
+
+	IEnumerator PlayLanternSound(){
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+		
 	}
 }
