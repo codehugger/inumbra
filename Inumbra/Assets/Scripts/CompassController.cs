25977ed76a1e6bc38f1a87b57a429768b5b9eb52
Dennis Maier <dennis.maier@tum.de>
2018-12-15 14:09:30 +0000

    compass points towards random coal and enemies idle when out of range

--- Inumbra/Assets/Scripts/CompassController.cs
+++ Inumbra/Assets/Scripts/CompassController.cs
@@ -6,35 +6,38 @@ using UnityEngine.UI;
 public class CompassController : MonoBehaviour {
 
 	public GameObject needle;
-
-	public float showAfterDistance;
+	
 	GameObject player;
-	GameObject train;
+
+	int coalCount;
+	int oldCount = 1000;
+	int currentCoal;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
-		train = GameObject.FindGameObjectWithTag("Train");
+		coalCount = GameObject.FindGameObjectsWithTag("coal").Length;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (player == null || train == null) { return; }
-
-		if (Vector3.Distance(player.transform.position, train.transform.position) > showAfterDistance){
-			Image[] list = gameObject.GetComponentsInChildren<Image>();
-			foreach (Image l in list)
-			{
-				l.enabled = true;
-			}
-		} else {
+		GameObject[] coals = GameObject.FindGameObjectsWithTag("coal");
+		coalCount = coals.Length;
+		if (player == null || coalCount == 0) { 
 			Image[] list = gameObject.GetComponentsInChildren<Image>();
 			foreach (Image l in list)
 			{
 				l.enabled = false;
 			}
+			return;
 		}
-		Vector3 rot = player.transform.position - train.transform.position;
+
+		if (coalCount != oldCount) {
+			currentCoal = Random.Range(0, coalCount);
+			oldCount = coalCount;
+		}
+
+		Vector3 rot = player.transform.position - coals[currentCoal].transform.position;
 		float look_angle = Mathf.Atan2(rot.y, rot.x) * Mathf.Rad2Deg;
         Quaternion new_rot = Quaternion.AngleAxis(look_angle, Vector3.forward);
 		needle.transform.rotation = new_rot;
